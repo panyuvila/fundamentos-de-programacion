6019c57b263ed49039f97ef16ec1240582cf9472
panyuvila <73607368+panyuvila@users.noreply.github.com>
2022-02-20 12:59:00 +0100

    carga hasta ejercicio 12

---
+++ index.js
@@ -0,0 +1,28 @@
+let variableSinValor()
+}
+let booleano1 = 10 > 9
+}
+let ,booleano2 = 1 > 9
+}
+const PI = 3.14
+const tau = 2*PI
+}
+const booleanoy = (booleano1 != booleano2')
+}
+let contarHasta10_2  = 0
+for ( ; contarHasta10_2 < 10)
+console log (contarHasta10_2)
+contarHasta10_2 = contarHasta10_2 + 1
+}
+let postI  = 0
+let postJ = 0
+for ( ;posti + postj < 10)
+postj++= postI + postJ
+console log (contarHasta10_2)
+}
+let sumaPares = 0
+for (var i=1; i<x; i++)
+console log (sumaPares < 10 =i)
+}
+
+
